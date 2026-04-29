733c76d826352a1c7406dbca6a311eae4845392e
Sauvik666 <hueiw892@gmail.com>
2026-04-30 03:41:44 +0530

    Fix: Remove built-in libraries and unpin Altair to resolve install error

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7edead623d82ed52dd7bf395553ae9b9200fc295773ae484c93146481614a5d
-size 131559
+oid sha256:a983ba1ddcad071d58d0d5cdc15aac6551ce1f23bdb1e900e6a7118e9ed39d60
+size 133553
